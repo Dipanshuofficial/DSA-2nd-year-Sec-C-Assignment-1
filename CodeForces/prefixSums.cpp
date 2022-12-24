9b28ae2cf002cd355b3184fa7b89e42fc91fcc56
Dipanshuofficial <114001896+Dipanshuofficial@users.noreply.github.com>
2022-12-24 14:30:06 +0530

    Update prefixSums.cpp

--- CodeForces/prefixSums.cpp
+++ CodeForces/prefixSums.cpp
@@ -1,10 +1,32 @@
-vector<int> prefixSums(vector<int> &num){
-    vector<int> ans;
-    int a=0;
-    int s=num.size();
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+vector<long> prefixSums(vector<long> &num){
+    vector<long> ans;
+    long a=0;
+    long s=num.size();
     ans.push_back(a);
-    for(int i=0; i<s; i++){
-        ans.push_back(ans.back()+num[i]);
+    for(long i=0; i<s; i++){
+        long temp=ans.back();
+        ans.push_back(temp+num[i]);
     }
     return ans;
+}
+void prlongvect(vector<long> &ans, long n){
+    for(long i=0; i<=n; i++){
+        cout<<ans[i]<<" ";
+    }
+}
+int main(){
+    long n;
+    cin>>n;
+    vector<long> juft, sol;
+    for(long i=0;i<n; i++){
+        long temp;
+        cin>>temp;
+        juft.push_back(temp);
+    }
+    sol=prefixSums(juft);
+    prlongvect(sol,n);    
+    return 0;
 }
