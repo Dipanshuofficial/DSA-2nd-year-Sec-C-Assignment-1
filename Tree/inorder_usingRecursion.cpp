d735337551c8c98028a97015f9f9860ab226d73a
Dipanshuofficial <114001896+Dipanshuofficial@users.noreply.github.com>
2023-01-21 18:33:40 +0530

    Create inorder_usingRecursion.cpp

---
+++ Tree/inorder_usingRecursion.cpp
@@ -0,0 +1,26 @@
+//LeetCode Solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& vec){
+        if(root==NULL) return;
+        inorder(root->left, vec);
+        vec.push_back(root->val);
+        inorder(root->right, vec);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        inorder(root, ans);
+        return ans;
+    }
+};
