d5adeb554a97c656e1984ed5ec59fee5dd1d998a
Dipanshuofficial <114001896+Dipanshuofficial@users.noreply.github.com>
2023-01-21 19:13:12 +0530

    Create postorder_usingRecursion.cpp

---
+++ Tree/postorder_usingRecursion.cpp
@@ -0,0 +1,26 @@
+//LeetCode Solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void postorder(TreeNode* root, vector<int>& vec){
+        if(root==NULL) return;
+        postorder(root->left, vec);
+        postorder(root->right, vec);
+        vec.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        postorder(root,ans);
+        return ans;
+    }
+};
